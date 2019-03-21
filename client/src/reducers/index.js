e0e479b0ff1c87aa33dfdefbafd632f2392078a6
Xi Wang <wangx272@mcmaster.ca>
2019-03-21 14:30:33 -0400

    Add dashboard & profile state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
-import setAuthToken from "../utils/setAuthToken";
-import { setCurrentUser } from "../actions/authActions";
+import profileReducer from "./profileReducer";
 
 export default combineReducers({
   auth: authReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  profile: profileReducer
 });
