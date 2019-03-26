eca0a36a1bfa716aaf032120759ed3c9c65591f0
Xi Wang <wangx272@mcmaster.ca>
2019-03-26 16:35:09 -0400

    Add Profile Display

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+class ProfileGithub extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientId: "ca2d1028665c83f2fde4",
+      clientSecret: "bb7dbfb3c9dd91abc0928fe2e3f5d95e7a856124",
+      count: 5,
+      sort: "created: asc",
+      repos: []
+    };
+  }
+
+  componentDidMount() {
+    const { username } = this.props;
+    const { count, sort, clientId, clientSecret } = this.state;
+
+    fetch(
+      `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ repos: data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { repos } = this.state;
+
+    const repoItems = repos.map(repo => (
+      <div key={repo.id} className="card card-body mb-2">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>
+              <Link to={repo.html_url} className="text-info" target="_blank">
+                {repo.name}
+              </Link>
+            </h4>
+            <p>{repo.description}</p>
+          </div>
+          <span className="badge badge-info mr-1">
+            Star:{repo.stargazers_count}
+          </span>
+          <span className="badge badge-secondary mr-1">
+            Watchers:{repo.watchers_count}
+          </span>
+          <span className="badge badge-success">Forks:{repo.forks_count}</span>
+        </div>
+      </div>
+    ));
+
+    return (
+      <div ref="myRef">
+        <hr />
+        <h3 className="mb-4">Latest Github Repos</h3>
+        {repoItems}
+      </div>
+    );
+  }
+}
+
+ProfileGithub.propTypes = {
+  username: PropTypes.string.isRequired
+};
+
+export default ProfileGithub;
